361fdb9d423d3710103174bd3eba510eb5ff64ba
roughcellophane <salupan2004@gmail.com>
2023-02-25 21:45:55 +0800

    git modified accounts.sql

--- accounts.sql
+++ accounts.sql
@@ -1,9 +1,26 @@
 CREATE TABLE Users (
-    UserID int,
+    UserID INT,
     LName varchar(255),
     FName varchar(255),
     Password varchar(255),
     Access varchar(255)
 )
 
-INSERT INTO 
+-- nasa accounts file tayo but lipat ko na lang ito after meeting 
+CREATE TABLE Menu (
+    ProdID VARCHAR(255),
+    ProdName varchar(255),
+    AvailBool boolean,
+    PriceValue int,
+    PrepTime int
+)
+
+CREATE TABLE Orders (
+    OrderID varchar(255),
+    CurrentStatus varchar(255),
+    UserID int,
+    PrepTime int
+)
+
+INSERT INTO hi idol
+
