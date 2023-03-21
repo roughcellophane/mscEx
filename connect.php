59389275bbf4955b8c98cc3b6b21a3a0828a1c98
roughcellophane <salupan2004@gmail.com>
2023-03-21 22:15:23 +0800

    git modified connect.php

--- connect.php
+++ connect.php
@@ -6,5 +6,11 @@
 
     mysql_connect($host,$user,$password)
     mysql_select_db($db)
-    
+
+    if(isset([UserID]){
+        $uname=$_POST['username'];
+        $password=$_POST['password'];
+        $sql="select * from accounts where UserID='".$uname."' and Password='".$password."' limit 1"
+        $result
+    }
 ?>
