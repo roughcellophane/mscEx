c41235fc99d1b17406204bfa045692638d1238f8
roughcellophane <salupan2004@gmail.com>
2023-03-21 22:15:40 +0800

    git modified connect.php

--- connect.php
+++ connect.php
@@ -7,10 +7,18 @@
     mysql_connect($host,$user,$password)
     mysql_select_db($db)
 
-    if(isset([UserID]){
+    if(isset($_POST[UserID]){
         $uname=$_POST['username'];
         $password=$_POST['password'];
-        $sql="select * from accounts where UserID='".$uname."' and Password='".$password."' limit 1"
-        $result
+        $sql="select * from accounts where UserID='".$uname."' and Password='".$password."' limit 1";
+        $result=mysql_query($sql);
+        if mysql_num_rows($result)==1{
+            echo "Log in Success!";
+            exit();
+        }
+        else{
+            echo "Pakshit ka talaga alam mo yun";
+            exit();
+        }
     }
 ?>
