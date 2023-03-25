dda96fb3c406a181645ad769d09bcd0a3b8b661f
roughcellophane <salupan2004@gmail.com>
2023-03-25 20:28:10 +0800

    Co-authored-by: LetsPlayLate <LetsPlayLate@users.noreply.github.com>

--- accounts.sql
+++ accounts.sql
@@ -4,9 +4,9 @@ CREATE TABLE Users (
     UserID int,
     LName varchar(255),
     FName varchar(255),
-    Password varchar(255),
+    Pass varchar(255),
     Access varchar(255)
-)
+    );
 
 CREATE TABLE Menu (
     ProdID VARCHAR(255),
@@ -14,26 +14,23 @@ CREATE TABLE Menu (
     AvailBool boolean,
     PriceValue int,
     PrepTime int
-)
+);
 
 CREATE TABLE Orders (
     OrderID varchar(255),
     CurrentStatus varchar(255),
     UserID int,
-    ProdID array,
-    ProdNum array,
+    ProdID varchar(255),
+    ProdNum varchar(255),
     CurrentTime int
-)
+);
 
--- data input
+
 INSERT INTO Menu (ProdID, ProdName, AvailBool, PriceValue, PrepTime)
-VALUES ROW("M2", "Fried Chicken", "TRUE", "70", "60"),
-       ROW(,,,,),
-       ;
+VALUES ROW ("M2", "Fried Chicken", "1", "70", "60");
 
-INSERT INTO Users (UserID, LName, FName, Password, Access)
-VALUES ROW("57647", "Italio", "Melody", "MItalio", "60"),
-       ROW(,,,,),
-       ;
+INSERT INTO Users (UserID, LName, FName, Pass, Access)
+VALUES ROW ("57647", "Italio", "Melody", "MItalio", "60");
 
 INSERT INTO Orders (OrderID, CurrentStatus, UserID, ProdID, ProdNum, CurrentTime)
+VALUES ROW ("434893", "SENT", "57467", "2", "1", "2");
