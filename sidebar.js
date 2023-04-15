13e0e4618241194e774cc67424e567620aad0d55
roughcellophane <salupan2004@gmail.com>
2023-04-15 20:34:41 +0800

    damfldf

--- sidebar.js
+++ sidebar.js
@@ -1,11 +1,12 @@
 
 function openNav_sidebar() {
-  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("mySidebar").style.left = "0px";
+  document.getElementById("mySidebar").style.width = "310px";
   document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav_sidebar() {
-  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("mySidebar").style.left = "-310px";
   document.getElementById("main").style.marginLeft = "0";
 }
 
