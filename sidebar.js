5272535eb7dd754149cc8c982a1bb78661b0ac3e
ScarlettBloom117 <124589071+ScarlettBloom117@users.noreply.github.com>
2023-04-21 19:08:19 +0800

    Add files via upload

--- sidebar.js
+++ sidebar.js
@@ -1,12 +1,11 @@
 
 function openNav_sidebar() {
   document.getElementById("mySidebar").style.left = "0px";
-  document.getElementById("mySidebar").style.width = "310px";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("sidebarButton").style.marginLeft = "250px";
 }
 
 function closeNav_sidebar() {
   document.getElementById("mySidebar").style.left = "-310px";
-  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("sidebarButton").style.marginLeft = "0";
 }
 
