ba9ed02d63f1761cbdbd71437608441435308d09
ScarlettBloom117 <124589071+ScarlettBloom117@users.noreply.github.com>
2023-04-30 18:29:49 +0800

    Add files via upload

---
+++ item details.js
@@ -0,0 +1,10 @@
+function openTonkatsu() {
+  document.getElementById("TONKATSU").style.height = "100%";
+  document.getElementById("TONKATSU").style.zIndex = "5";
+  document.getElementById("escape").style.top = "25px";
+  document.getElementById("logo").style.left = "15px";
+}
+
+function closeTonkatsu() {
+  document.getElementById("TONKATSU").style.height = "0%";
+}
