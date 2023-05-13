ebb60202466bb8266442ab5f1e0af381befb79cd
roughcellophane <salupan2004@gmail.com>
2023-05-13 16:51:33 +0800

    modified:   client.php 	modified:   server.php

--- client.php
+++ client.php
@@ -1,27 +1,18 @@
 <?php
-
-$sock = socket_create_listen(0);
-
-socket_getsockname($sock, $addr, $port);
-
-print "Server Listening on $addr:$port\n";
-
-$fp = fopen($port_file, 'w');
-
-fwrite($fp, $port);
-
-fclose($fp);
-
-while($c = socket_accept($sock)) {
-
-   /* do something useful */
-
-   socket_getpeername($c, $raddr, $rport);
-
-   print "Received Connection from $raddr:$rport\n";
-
-}
-
-socket_close($sock);
+$host    = "127.0.0.1";
+$port    = 25003;
+$message = "Hello Server";
+echo "Message To server :".$message;
+// create socket
+$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
+// connect to server
+$result = socket_connect($socket, $host, $port) or die("Could not connect to server\n");  
+// send string to server
+socket_write($socket, $message, strlen($message)) or die("Could not send data to server\n");
+// get server response
+$result = socket_read ($socket, 1024) or die("Could not read server response\n");
+echo "Reply From Server  :".$result;
+// close socket
+socket_close($socket);
 
 ?>
