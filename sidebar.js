19805bd38ad3093bf564a96dce3ebfa775b6c259
roughcellophane <salupan2004@gmail.com>
2023-05-14 22:24:36 +0800

    Co-authored-by: ScarlettBloom117 <ScarlettBloom117@users.noreply.github.com>

--- sidebar.js
+++ sidebar.js
@@ -1,11 +1,11 @@
 
 function openNav_sidebar() {
   document.getElementById("mySidebar").style.left = "0px";
-  document.getElementById("sidebarButton").style.marginLeft = "250px";
+  document.getElementById("sidebarButton").style.marginLeft = "248px";
 }
 
 function closeNav_sidebar() {
-  document.getElementById("mySidebar").style.left = "-310px";
+  document.getElementById("mySidebar").style.left = "-312px";
   document.getElementById("sidebarButton").style.marginLeft = "0";
 }
 
