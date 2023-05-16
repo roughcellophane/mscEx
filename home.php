571187a61902ddb5e2f1e2bb16874b4f425f9721
roughcellophane <salupan2004@gmail.com>
2023-05-16 13:44:56 +0800

    tite

--- home.php
+++ home.php
@@ -1,19 +1,7 @@
 <?php
-//Get Heroku ClearDB connection information
+
 session_start();
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
-
 include('db_conn.php');
-include('http_response_code.php');
 $id = $_SESSION['UserID'];
 $query = "SELECT * FROM Orders WHERE UserID = '$id'"; 
 $result = mysqli_query($conn, $query);
